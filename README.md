893c292e0ea258a9ed573d57660a7f6567b0b633
Roman <r.alexeychenko@mmdsmart.com>
2019-02-27 16:44:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-webpack-babel
+
+## Features
+
+* React 16
+* Webpack 4
+* Babel 7
+* Hot Module Replacement
+
+## Installation
+
+* yarn install
+* yarn start
+* visit `http://localhost:8080/`
+
+## Author
+
+* **Roman Alexeychenko** - [romanmtb](https://github.com/romanmtb)
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            React Webpack Babel Setup Boilerplate
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
